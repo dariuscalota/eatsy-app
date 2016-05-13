ee55f21c2157562cc3d11e9d53960b5f88638a56
Darius Calota <darius.calota@gmail.com>
2016-05-13 18:14:49 +0300

    test 2

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8000);
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
