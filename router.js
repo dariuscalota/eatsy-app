50d66ab3249ee8c1c11cb3581bfa89e4f8d7b3e7
sebastian oprea <sebastian oprea>
2016-05-14 12:45:44 +0300

    fix

--- router.js
+++ router.js
@@ -12,7 +12,6 @@ module.exports = function(app) {
 
   app.get('/api/users', requireAuth, function(req, res) {
       User.find({}, function(err, users) {
-       delete user[password];
         res.json(users);
     });
   });
@@ -35,7 +34,6 @@ module.exports = function(app) {
   });
   app.get('/api/users/:id', function(req, res) {
      User.find({'_id': req.params.id}, function(err, user) {
-       delete user[password];
        res.json(user);
      });
    });
